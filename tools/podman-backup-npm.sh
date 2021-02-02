e4d6e742f9e639cb6e54f9104962540f9c57174f
ahwayakchih <ahwayakchih@gmail.com>
2021-02-02 20:41:51 +0100

    fix: backup failing because of NPM container
    We do not want to backup anything from NPM, but default script
    is looking for CONTAINER_DATA_DIR, which is not set for NPM container.
    So add empty script, that will simply exit OK, so backup can succeed.

---
+++ tools/podman-backup-npm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# WARNING: This script has to be run OUTSIDE container.
+#          It's meant to create backup of containers' data.
+
+# Do nothing - we do not want to backup NPM data
