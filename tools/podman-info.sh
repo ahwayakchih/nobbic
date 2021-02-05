1e90c701e8e1038b74f1a03c4ea1caffd2b67b2f
ahwayakchih <ahwayakchih@gmail.com>
2021-02-05 08:17:29 +0100

    feat: add host OS name and podman version to output of info action
    Also improve text.
    Also change to call `podman container inspect` not `podman inspect`

--- tools/podman-info.sh
+++ tools/podman-info.sh
@@ -17,12 +17,13 @@ if ! podman pod exists ${APP_NAME} ; then
 	exit 1
 fi
 
-NODEBB_ENV=$(podman inspect "${APP_NAME}-nodebb" --format='{{range .Config.Env}}{{.}}\n{{end}}' | grep -E "^(NODE(BB)?|APP_USE)_")
+NODEBB_ENV=$(podman container inspect "${APP_NAME}-nodebb" --format='{{range .Config.Env}}{{.}}\n{{end}}' | grep -E "^(NODE(BB)?|APP_USE)_")
 NODE_VERSION=$(echo "$NODEBB_ENV" | grep NODE_VERSION | cut -d= -f2 || echo "")
 NODEBB_VERSION=$(echo "$NODEBB_ENV" | grep NODEBB_VERSION | cut -d= -f2 || echo "")
 NODEBB_PORT=$(echo "$NODEBB_ENV" | grep -E "^APP_USE_PORT=" | cut -d= -f2 || echo "")
 NODEBB_FQDN=$(echo "$NODEBB_ENV" | grep -E "^APP_USE_FQDN=" | cut -d= -f2 || echo "")
 
+echo "Hosted on "$(source /etc/os-release && echo $PRETTY_NAME)" using Podman v"$(podman version | head -n 1 | tr -d '[:blank:]' | cut -d : -f2)
 echo "NodeBB v${NODEBB_VERSION} is run with Node.js v${NODE_VERSION}"
 echo "Built with Containerized-NodeBB v"$(podman pod inspect "$APP_NAME" --format='{{range $key,$value := .Labels}}{{$key}}={{$value}}\n{{end}}' | grep "$CONTAINERIZED_NODEBB_LABEL" | cut -d= -f2 || echo "unknown")
 echo "It uses:"
@@ -32,6 +33,7 @@ for container in $(podman pod inspect "$APP_NAME" --format='{{range .Containers}
 	image=$(podman container inspect "$container" --format='{{.ImageName}}' || echo "")
 
 	echo "- $name ($image)"
+	echo "  with "$(podman container inspect "$container" --format='{{range .Config.Env}}{{.}}\n{{end}}' | grep '_VERSION' | xargs echo)
 done
 
 
@@ -39,7 +41,7 @@ isRunning=$(podman pod ps --filter status=running --filter name="$APP_NAME" -q)
 if test $isRunning ; then
 	echo -n "Awaits "
 else
-	echo -n "Will await "
+	echo -n "When started, it will await "
 fi
 # TODO: check if gateway uses SSL or not
 echo "connections at https://${NODEBB_FQDN}:${NODEBB_PORT}"
