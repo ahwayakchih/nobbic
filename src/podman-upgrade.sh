ed731a006c52449602ecd8703765645442764ade
ahwayakchih <ahwayakchih@gmail.com>
2021-02-12 14:55:14 +0100

    fix: upgrading will now create backup in usual place instead of /tmp
    This allows to restore to it manually, and makes it easier to debug.

--- src/podman-upgrade.sh
+++ src/podman-upgrade.sh
@@ -11,20 +11,19 @@ if ! podman pod exists ${APP_NAME} ; then
 fi
 
 if [ -z "$NODEBB_VERSION" ] ; then
-    echo "WARNING: NODEBB_VERSION was not specified, so upgrade will try latest released version" >&2
-    NODEBB_VERSION="latest"
+    export NODEBB_VERSION="latest"
+    echo "WARNING: NODEBB_VERSION was not specified, so upgrade will try ${NODEBB_VERSION} version" >&2
 fi
 
 if [ -z "$NODE_VERSION" ] ; then
-    echo "WARNING: NODE_VERSION was not specified, so upgrade will use either version used in backup or minimal Node.js version required by selected NodeBB (whichever is higher)" >&2
+    echo "WARNING: NODE_VERSION was not specified, so upgrade will use either version used currently or minimal Node.js version required by selected NodeBB (whichever is higher)" >&2
 fi
 
 NOW=$(date -u +%Y-%m-%dT%H-%M-%S)
-BACKUP_NAME="${APP_NAME}_${NOW}"
+export BACKUP_NAME=${BACKUP_NAME:-${APP_NAME}_${NOW}_before_upgrade}
 
-${__APP} stop $APP_NAME || return 1
-${__APP} backup $APP_NAME /tmp "$BACKUP_NAME" || (${__APP} start $APP_NAME && exit 1) || return 1
+env ${__APP} stop $APP_NAME || return 1
+env ${__APP} backup $APP_NAME || (${__APP} start $APP_NAME && exit 1) || return 1
 # TODO: when podman supports renaming, create something like APP-upgrade first, test and if all ok, remove old and rename new pod and containers
 ${__APP} remove $APP_NAME || return 1
-env NODE_VERSION="$NODE_VERSION" NODEBB_VERSION="$NODEBB_VERSION" ${__APP} restore $APP_NAME "/tmp/${BACKUP_NAME}" || ${__APP} restore $APP_NAME "/tmp/${BACKUP_NAME}" || return 1
-rm -rf "/tmp/${BACKUP_NAME}">/dev/null || true
+env NODE_VERSION="$NODE_VERSION" NODEBB_VERSION="$NODEBB_VERSION" ${__APP} restore $APP_NAME || ${__APP} restore $APP_NAME || return 1
