f54b9fd8e49c7acf155fdaa3c3dd1d0675b4b715
ahwayakchih <ahwayakchih@gmail.com>
2021-02-14 16:09:49 +0100

    fix: installing service for OpenRC
    Restarting to see if service existed before did not seem to work well:
    pod was not running after reboot (it looked like it was started and stopped?).
    
    So now we check if it exists, add it if not, and then restart in any case.
    
    Thanks to @nhlpl for reports.

--- src/host-install-service-openrc.sh
+++ src/host-install-service-openrc.sh
@@ -13,7 +13,7 @@ echo "Service file for $APP_NAME was created as $serviceFile"
 if command su --help &>/dev/null ; then
 	echo "Trying to install it now as root user..."
 	([ ! -f "$unitFile" ] && exit 0 || (read -r -p "'$unitFile' exists. Overwrite? [n/Y] " confirmation && [ "$confirmation" = 'Y' ] || exit 1))\
-		&& (su -c "cp -aT '$serviceFile' '$unitFile' && (rc-service $APP_NAME restart || (rc-update add $APP_NAME && rc-service $APP_NAME start))"\
+		&& (su -c "cp -aT '$serviceFile' '$unitFile' && ((rc-service -e $APP_NAME || rc-update add $APP_NAME) && rc-service $APP_NAME restart)"\
 			&& rm $serviceFile\
 			&& echo "Done"\
 			|| echo "Failed" && exit 1)\
